1dc52e9d71aca957d9c166277e3703153cfee82a
ItayGold <itil.gold@gmail.com>
2021-02-15 11:39:05 +0200

    currency exchange - zipkin implementation

--- currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,6 +1,8 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+  private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
   @Autowired private CurrencyExchangeRepository repository;
   @Autowired private Environment environment;
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyExchange retrieveExchangeValue(
       @PathVariable String from, @PathVariable String to) {
+
+    logger.info("retrieve Exchange Value called with {} to {}", from, to);
+
     CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
 
     if (currencyExchange == null) {
